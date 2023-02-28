af3f0a5bafb5e91c41fa7edde2896b2904e1868f
bart-inho <barth.anhorn@icloud.com>
2023-02-28 14:24:35 +0100

    restart

--- module.f90
+++ module.f90
@@ -8,11 +8,11 @@ module all
     type everything
 
         ! Everything is a type that contains all the variables and subroutines needed for the simulation
-        integer :: i, j, nx, ny, nz, sou_position, ice_position, rock_position
+        integer :: i, j, nx, sou_position, ice_position, rock_position
         real(8) :: c, mu0 = 1.256e-6, eps0 = 8.854e-12, imp, sigma_max
         real(8) :: lambda_min = 12., dz, dt, t0, tau = 30.0, w0, pi = 4*TAN(1.0d0)
         real(8) :: ice_permittivity = 3.2, rock_permittivity = 5.0, air_permittivity = 1.0
-        real(8), allocatable :: epr(:, :), mur(:, :), Ex(:, :), Ey(:, :), Hx(:, :), Hy(:, :)
+        real(8), allocatable :: epr(:), mur(:), Ex(:), Hz(:)
 
     contains
 
@@ -28,35 +28,30 @@ contains
         implicit none
         class(everything), intent(inout) :: this
 
-        this%nx = 500 ! Number of points
-        this%ny = 500 ! Number of points
-        this%sou_position = 250 ! Source position
+        this%nx = 5000 ! Number of points
+        this%sou_position = 2500 ! Source position
         this%c = sqrt(1/(this%mu0*this%eps0)) ! Speed of light in free space
         this%imp = sqrt(this%mu0/this%eps0) ! Impedance of free space
         this%dz = this%lambda_min/20 ! 20 points per wavelength
         this%dt = this%dz/this%c ! Time step
         
-        allocate(this%epr(this%nx, this%ny), this%mur(this%nx+1, this%ny+1)) ! Allocate arrays
-        allocate(this%Hx(this%nx+1, this%ny+1), this%Hy(this%nx, this%ny)) ! Allocate arrays
-        allocate(this%Ex(this%nx, this%ny), this%Ey(this%nx+1, this%ny+1)) ! Allocate arrays
+        allocate(this%epr(this%nx), this%mur(this%nx+1), this%Ex(this%nx), this%Hz(this%nx+1)) ! Allocate arrays
         
         this%epr = 1.0 ! Set all relative permittivities to air
         this%mur = 1.0 ! Set all relative permeabilities to free space
         this%Ex = 0.0 ! Set all electric fields to zero
-        this%Hy = 0.0 ! Set all magnetic fields to zero
-        this%Ey = 0.0 ! Set all electric fields to zero
-        this%Hx = 0.0 ! Set all magnetic fields to zero
+        this%Hz = 0.0 ! Set all magnetic fields to zero
         
         this%w0 = 2.0*this%pi*this%c/this%lambda_min ! Angular frequency
         this%t0 = this%tau * 6. ! Time delay
         
         this%ice_position = 3000
         this%rock_position = 3500
-        ! this%epr(this%ice_position:this%nx) = this%ice_permittivity ! Set permittivity of ice
-        ! this%epr(this%rock_position:this%nx) = this%rock_permittivity ! Set permittivity of rock
+        this%epr(this%ice_position:this%nx) = this%ice_permittivity ! Set permittivity of ice
+        this%epr(this%rock_position:this%nx) = this%rock_permittivity ! Set permittivity of rock
 
-        open(45, file='Ex.txt', status='old') ! Open text files
-        open(29, file='Hy.txt', status='old') ! Open text files
+        open(45, file='Ex.txt', status='new') ! Open text files
+        open(29, file='Hz.txt', status='new') ! Open text files
 
     end subroutine EverythingInit
 
@@ -64,7 +59,7 @@ contains
         implicit none
         class(everything), intent(inout) :: this
 
-        deallocate(this%epr, this%mur, this%Ex, this%Hy) ! Deallocate arrays
+        deallocate(this%epr, this%mur, this%Ex, this%Hz) ! Deallocate arrays
 
         close(45) ! Close text files
         close(29) ! Close text files
@@ -77,7 +72,7 @@ contains
         class(everything) :: this
 
         write(45,'(100000(1G13.3e3))') this%Ex ! Write Ex to Ex.txt
-        write(29,'(100000(1G13.3e3))') this%Hy ! Write Hy to Hy.txt
+        write(29,'(100000(1G13.3e3))') this%Hz ! Write Hz to Hz.txt
 
     end subroutine WriteToText
 
@@ -87,26 +82,19 @@ contains
         integer, intent(in) :: t
 
         ! Gaussian source
-        this%Ex(this%sou_position, this%sou_position) = this%Ex(this%sou_position, this%sou_position) &
-            + exp(-(t-this%t0)**2/this%tau**2) * sin(this%w0*t*this%dt)/this%imp
-        this%Hy(this%sou_position, this%sou_position) = this%Hy(this%sou_position, this%sou_position) &
-            - exp(-(t-this%t0)**2/this%tau**2) * sin(this%w0*t*this%dt)/this%imp
+        this%Ex(this%sou_position) = this%Ex(this%sou_position) + exp(-(t-this%t0)**2/this%tau**2) * sin(this%w0*t*this%dt)/this%imp
+        this%Hz(this%sou_position) = this%Hz(this%sou_position) - exp(-(t-this%t0)**2/this%tau**2) * sin(this%w0*t*this%dt)/this%imp
 
     end subroutine GaussianSource
     
     subroutine UpdateMagneticField(this) ! Update magnetic field
         implicit none
         class(everything), intent(inout) :: this
-        integer :: i = 0, j = 0
+        integer :: i = 0
 
-        do concurrent(i = 2:this%nx, j = 1:this%ny) ! Update Hx
-            this%Hx(i, j) = this%Hx(i, j) + this%dt / (this%mu0 * this%mur(i, j))*(this%Ey(i, j) - this%Ey(i, j-1)) / this%dz
+        do i = 2, this%nx ! Update Hz
+            this%Hz(i) = this%Hz(i) + this%dt / (this%mu0 * this%mur(i))*(this%Ex(i) - this%Ex(i-1)) / this%dz
         end do
-
-        do concurrent(i = 1:this%nx, j = 1:this%ny) ! Update Hy
-            this%Hy(i, j) = this%Hy(i, j) + this%dt / (this%mu0 * this%mur(i, j))*(this%Ex(i, j) - this%Ex(i-1, j)) / this%dz
-        end do
-
         
         call BoundaryConditions(this) ! Apply boundary conditions
 
@@ -115,15 +103,10 @@ contains
     subroutine UpdateElectricField(this) ! Update electric field
         implicit none
         class(everything), intent(inout) :: this
-        integer :: i = 0, j = 0
+        integer :: i = 0
         
-        
-        do concurrent(i = 1:this%nx, j = 1:this%ny) ! Update Ex
-            this%Ex(i, j) = this%Ex(i, j) + this%dt/(this%eps0 * this%epr(i, j))*(this%Hy(i+1, j) - this%Hy(i, j)) / this%dz
-        end do
-
-        do concurrent(i = 2:this%nx, j = 2:this%ny) ! Update Ey
-            this%Ey(i, j) = this%Ey(i, j) + this%dt/(this%eps0 * this%epr(i, j))*(this%Hx(i, j+1) - this%Hx(i, j)) / this%dz
+        do i = 1, this%nx ! Update Ex
+            this%Ex(i) = this%Ex(i) + this%dt/(this%eps0 * this%epr(i))*(this%Hz(i+1) - this%Hz(i)) / this%dz
         end do
         
         ! No boundary conditions for Ex due to staggered grid stencil
@@ -135,10 +118,8 @@ contains
         class(everything), intent(inout) :: this
 
         ! Boundary conditions
-        this%Hy(1, :) = this%Hy(2, :) ! Set Hy at the left boundary to the value of the second boundary
-        this%Hy(this%nx, :) = this%Hy(this%nx-1, :) ! Set Hy at the right boundary to the value of the second to right boundary
-        this%Hy(:, 1) = this%Hy(:, 2) ! Set Hy at the bottom boundary to the value of the second boundary
-        this%Hy(this%nx+1, :)   = this%Hy(this%nx, :) ! Set Hy at the second to right boundary to the value of the third to right boundary
+        this%Hz(1) = this%Hz(2) ! Set Hz at the left boundary to the value of the second boundary
+        this%Hz(this%nx+1)   = this%Hz(this%nx) ! Set Hz at the second to right boundary to the value of the third to right boundary
 
     end subroutine BoundaryConditions
     
